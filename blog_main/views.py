1b66a3c8ae6a13638d16e1bbd6e5038b5cff1151
Prathamesh <prathameshshinde2254@gmail.com>
2026-06-20 18:45:35 +0530

    Backend Connection updates

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from blogs.models import Category, Blog
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    featured_blogs = Blog.objects.filter(is_featured=True, status='Published').order_by('-created_at')[:5]
+    context = {
+        'categories': categories,
+        'featured_blogs': featured_blogs
+    }
+    return render(request, 'home.html', context)
+    
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Category, Blog
+from django.http import HttpResponse
 # Create your views here.
+
+
+def posts_by_category(request, category_id):
+    posts = Blog.objects.filter(category_id=category_id, status='Published')
+    '''try:
+
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')  # Redirect to home if category not found:
+    '''
+    #category = Category.objects.get(pk=category_id)
+    category = get_object_or_404(Category, pk=category_id)
+
+    context = {
+        'category': category,
+        'posts': posts
+    }
+    return render(request, 'posts_by_category.html', context)
+    #return HttpResponse(posts)
